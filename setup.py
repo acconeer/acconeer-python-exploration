9cedd5262348858d505ee65674d2974f3af0b862
Erik Månsson <erik.mansson@acconeer.com>
2019-06-07 13:14:26 +0200

    Reg protocol fix
    Change-Id: I9b37aa47a85051a87c9096b3a68c8882b14c6227

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ PACKAGE_ROOT_DIR = "lib"
 
 setup(
     name="acconeer-utils",
-    version="2.3.9",
+    version="2.3.10",
     description="Acconeer utilities",
     url="https://github.com/acconeer/acconeer-python-exploration",
     author="Acconeer AB",
