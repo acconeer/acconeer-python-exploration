9040f4605d89a1395cc1aa5dc79e1553a62cbdf0
Erik Månsson <erik.mansson@acconeer.com>
2019-11-18 09:48:56 +0100

    v3: Update JSON-based client for RSS v2
    Change-Id: I2520fb0fd4e855bed17a149d8cbad4de8c80267e

--- src/acconeer/exptool/clients/json/client.py
+++ src/acconeer/exptool/clients/json/client.py
@@ -1,12 +1,14 @@
-from time import time
-from copy import deepcopy
+import enum
+import json
 import logging
-from distutils.version import StrictVersion
+from copy import deepcopy
+from time import time
 
-from acconeer.exptool.clients.base import BaseClient, ClientError
+import numpy as np
+
 from acconeer.exptool.clients import links
-from acconeer.exptool.clients.json import protocol
-from acconeer.exptool.clients.base import decode_version_str
+from acconeer.exptool.clients.base import BaseClient, ClientError, decode_version_str
+from acconeer.exptool.modes import Mode, get_mode
 
 
 log = logging.getLogger(__name__)
@@ -20,7 +22,9 @@ class SocketClient(BaseClient):
 
         self._session_cmd = None
         self._session_ready = False
-        self._num_subsweeps = None
+        self._mode = None
+        self._sweeps_per_frame = None
+        self._num_sensors = None
 
     def _connect(self):
         info = {}
@@ -51,13 +55,9 @@ class SocketClient(BaseClient):
         server_version_str = msg[len(startstr):].strip()
         info.update(decode_version_str(server_version_str))
 
-        if info["strict_version"] >= StrictVersion("1.10"):
-            cmd = {"cmd": "get_board_sensor_count"}
-            self._send_cmd(cmd)
-            header, _ = self._recv_frame()
-            msg = header["message"]
-            board_sensor_count = int(msg)
-            info["board_sensor_count"] = board_sensor_count
+        self._send_cmd({"cmd": "get_board_sensor_count"})
+        header, _ = self._recv_frame()
+        info["board_sensor_count"] = int(header["message"])
 
         return info
 
@@ -66,7 +66,15 @@ class SocketClient(BaseClient):
             cmd = deepcopy(config)
             log.warning("setup with raw dict config - you're on your own")
         else:
-            cmd = protocol.get_dict_for_config(config)
+            cmd = get_dict_for_config(config)
+
+        try:
+            self._mode = get_mode(cmd["cmd"].lower().replace("_data", ""))
+        except ValueError:
+            self._mode = None
+
+        self._sweeps_per_frame = cmd.get("sweeps_per_frame", 16)
+        self._num_sensors = len(cmd["sensors"])
 
         cmd["output_format"] = "json+binary"
 
@@ -98,7 +106,9 @@ class SocketClient(BaseClient):
         elif status != "ok":
             raise ClientError("server error")
 
-        return protocol.decode_stream_frame(header, payload, self.squeeze, self._num_subsweeps)
+        info = self._decode_stream_header(header)
+        data = self._decode_stream_payload(payload)
+        return info, data
 
     def _stop_session(self):
         cmd = {"cmd": "stop_streaming"}
@@ -146,21 +156,139 @@ class SocketClient(BaseClient):
         log.debug("session initialized")
 
         self._session_ready = True
-        info = protocol.get_session_info_for_header(header)
-        self._num_subsweeps = info.get("number_of_subsweeps")
+        info = get_session_info_for_header(header)
         return info
 
     def _send_cmd(self, cmd_dict):
-        cmd_dict["api_version"] = 2
-        packed = protocol.pack(cmd_dict)
+        cmd_dict["api_version"] = 3
+        s = json.dumps(cmd_dict, separators=(",", ":"))
+        packed = bytearray(s + "\n", "ascii")
+
         self._link.send(packed)
 
     def _recv_frame(self):
-        packed_header = self._link.recv_until(b'\n')
-        header = protocol.unpack(packed_header)
+        packed = self._link.recv_until(b'\n')
+        header = json.loads(str(packed, "ascii"))
         payload_len = header["payload_size"]
+
         if payload_len > 0:
             payload = self._link.recv(payload_len)
         else:
             payload = None
+
         return header, payload
+
+    def _decode_stream_header(self, header):
+        raw_infos = header["result_info"]
+        mapped_infos = [{} for _ in raw_infos]
+
+        for (raw_info, mapped_info) in zip(raw_infos, mapped_infos):
+            for raw_key, val in raw_info.items():
+                mapped_key = DATA_HEADER_TO_INFO_KEY_REMAP.get(raw_key, raw_key)
+
+                if mapped_key is None:
+                    continue
+
+                mapped_info[mapped_key] = val
+
+        if self.squeeze and len(mapped_infos) == 1:
+            return mapped_infos[0]
+        else:
+            return mapped_infos
+
+    def _decode_stream_payload(self, payload):
+        if not payload:
+            return None
+
+        squeeze = self.squeeze and self._num_sensors == 1
+
+        if self._mode == Mode.SPARSE:
+            data = np.frombuffer(payload, dtype=">u2").astype("float")
+
+            if squeeze:
+                shape = (self._sweeps_per_frame, -1)
+            else:
+                shape = (self._num_sensors, self._sweeps_per_frame, -1)
+
+            return data.reshape(shape)
+
+        if self._mode == Mode.IQ:
+            data = np.frombuffer(payload, dtype=">i2").astype("float")
+            data = data.reshape((-1, 2)).view(dtype="complex").flatten()
+        elif self._mode in (Mode.ENVELOPE, Mode.POWER_BINS):
+            data = np.frombuffer(payload, dtype=">u2").astype("float")
+        else:  # Fallback
+            data = np.frombuffer(payload, dtype=">u2")
+
+        if not squeeze:
+            data = data.reshape((self._num_sensors, -1))
+
+        return data
+
+
+def get_dict_for_config(config):
+    d = {}
+    d["cmd"] = get_mode(config.mode).value.lower() + "_data"
+
+    for config_key, cmd_key in CONFIG_TO_CMD_KEY_MAP.items():
+        config_val = getattr(config, config_key, None)
+
+        if config_val is None:
+            continue
+
+        if isinstance(config_val, bool):
+            cmd_val = int(config_val)
+        elif isinstance(config_val, enum.Enum):
+            if hasattr(config_val, "json_value"):
+                cmd_val = config_val.json_value
+            else:
+                cmd_val = config_val.value
+        else:
+            cmd_val = config_val
+
+        d[cmd_key] = cmd_val
+
+    return d
+
+
+def get_session_info_for_header(header):
+    info = {}
+
+    for header_key, val in header.items():
+        info_key = SESSION_HEADER_TO_INFO_KEY_REMAP.get(header_key, header_key)
+
+        if info_key is None:
+            continue
+
+        info[info_key] = val
+
+    return info
+
+
+CONFIG_TO_CMD_KEY_MAP = {
+    "sensor": "sensors",
+    "range_start": "range_start",
+    "range_length": "range_length",
+    "repetition_mode": "repetition_mode",
+    "update_rate": "update_rate",
+    "gain": "gain",
+    "hw_accelerated_average_samples": "hw_accelerated_average_samples",
+    "profile": "profile",
+    "bin_count": "bin_count",
+    "downsampling_factor": "downsampling_factor",
+    "noise_level_normalization": "noise_level_normalization",
+    "running_average_factor": "running_average_factor",
+    "sampling_mode": "sampling_mode",
+    "sweeps_per_frame": "sweeps_per_frame",
+    "sweep_rate": "sweep_rate",
+}
+
+SESSION_HEADER_TO_INFO_KEY_REMAP = {
+    "status": None,
+    "payload_size": None,
+    "start_m": "range_start_m",
+    "length_m": "range_length_m",
+}
+
+DATA_HEADER_TO_INFO_KEY_REMAP = {
+}
