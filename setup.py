47a7c23227cffe6599646731cfba2c2d2f57ebc0
Erik Månsson <erik.mansson@acconeer.com>
2019-11-18 09:48:56 +0100

    v3: Update setup.py and add MANIFEST.in
    This also removes the previously added cleaning. The reason for that is
    that it breaks the setup when using pip to install. The new recommended
    way to do a clean install is to use a tool such as setupext-janitor. It
    can (currently) be installed with pip. Using it, a clean install can be
    done with:
    
    python setup.py clean --all install --user
    
    For reference, see the PyPA sample project:
    https://github.com/pypa/sampleproject
    and the Python packaging guidelines:
    https://packaging.python.org
    
    Change-Id: Iec2f957183edf79f2b9b289156aebca8465e20bc

--- setup.py
+++ setup.py
@@ -1,28 +1,11 @@
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-from shutil import rmtree
 import os
-import sys
 
 
-def clean():
-    for d in ["build", "dist", "lib/acconeer_utils.egg-info"]:
-        rmtree(d, True)
+project_dir = os.path.dirname(os.path.realpath(__file__))
+version_init_file = os.path.join(project_dir, "src/acconeer/exptool/__init__.py")
 
-
-class InstallCommand(install):
-    def run(self):
-        clean()
-        super().run()
-        clean()
-
-
-PACKAGE_ROOT_DIR = "lib"
-
-project_root_dir = os.path.dirname(os.path.realpath(__file__))
-root_init_file = os.path.join(project_root_dir, "lib/acconeer_utils/__init__.py")
-
-with open(root_init_file, "r") as f:
+with open(version_init_file, "r") as f:
     lines = f.readlines()
 
 for line in lines:
@@ -30,21 +13,20 @@ for line in lines:
         version = line.split("=")[1].strip()[1:-1]
         break
 else:
-    sys.stderr.write("Could not find the version number\n")
-    sys.exit(1)
+    raise Exception("Could not find the version")
+
+subpackages = find_packages(where="src/acconeer")
+packages = ["acconeer." + sp for sp in subpackages]
 
 setup(
-    name="acconeer-utils",
+    name="acconeer-exptool",
     version=version,
-    description="Acconeer utilities",
     url="https://github.com/acconeer/acconeer-python-exploration",
     author="Acconeer AB",
     author_email="github@acconeer.com",
     license="BSD",
-    package_dir={"": PACKAGE_ROOT_DIR},
-    packages=find_packages(PACKAGE_ROOT_DIR),
+    package_dir={"": "src"},
+    packages=packages,
     zip_safe=False,
-    package_data={"": ["bin/LICENSE.txt", "bin/*.so*", "bin/*/*.dll"]},
     include_package_data=True,
-    cmdclass={"install": InstallCommand},
 )
