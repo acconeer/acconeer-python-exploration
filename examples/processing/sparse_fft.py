f463fb7df8e4f985c6e66e5fe1c84f1ba2890020
Erik Månsson <erik.mansson@acconeer.com>
2019-11-21 21:53:04 +0100

    v3: Update sparse_fft.py
    Change-Id: Ic1c5394ea9d923175255406bd94c551cb4b5f57f

--- examples/processing/sparse_fft.py
+++ examples/processing/sparse_fft.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pyqtgraph as pg
 
+from acconeer.exptool import configs, utils
 from acconeer.exptool.clients import SocketClient, SPIClient, UARTClient
-from acconeer.exptool import configs
-from acconeer.exptool import utils
-from acconeer.exptool.pg_process import PGProcess, PGProccessDiedException
+from acconeer.exptool.pg_process import PGProccessDiedException, PGProcess
 from acconeer.exptool.structs import configbase
 
 
@@ -38,8 +37,8 @@ def main():
     processor = Processor(sensor_config, processing_config, session_info)
 
     while not interrupt_handler.got_signal:
-        info, sweep = client.get_next()
-        plot_data = processor.process(sweep)
+        info, data = client.get_next()
+        plot_data = processor.process(data)
 
         if plot_data is not None:
             try:
@@ -54,15 +53,12 @@ def main():
 
 def get_sensor_config():
     config = configs.SparseServiceConfig()
+    config.profile = configs.SparseServiceConfig.Profile.PROFILE_3
+    config.sampling_mode = configs.SparseServiceConfig.SamplingMode.A
     config.range_interval = [0.24, 0.48]
-    config.sampling_mode = config.SAMPLING_MODE_A
-    config.number_of_subsweeps = 64
-    config.subsweep_rate = 3e3
-
-    # max frequency
-    config.sweep_rate = 100
-    config.experimental_stitching = True
-
+    config.sweeps_per_frame = 64
+    config.sweep_rate = 3e3
+    config.hw_accelerated_average_samples = 60
     return config
 
 
@@ -70,18 +66,18 @@ class ProcessingConfiguration(configbase.ProcessingConfig):
     VERSION = 1
 
     show_data_plot = configbase.BoolParameter(
-            label="Show data",
-            default_value=True,
-            updateable=True,
-            order=0,
-            )
+        label="Show data",
+        default_value=True,
+        updateable=True,
+        order=0,
+    )
 
     show_speed_plot = configbase.BoolParameter(
-            label="Show speed on FFT y-axis",
-            default_value=False,
-            updateable=True,
-            order=10,
-            )
+        label="Show speed on FFT y-axis",
+        default_value=False,
+        updateable=True,
+        order=10,
+    )
 
 
 get_processing_config = ProcessingConfiguration
@@ -91,13 +87,13 @@ class Processor:
     def __init__(self, sensor_config, processing_config, session_info):
         pass
 
-    def process(self, sweep):
-        zero_mean_sweep = sweep - sweep.mean(axis=0, keepdims=True)
-        fft = np.fft.rfft(zero_mean_sweep.T * np.hanning(sweep.shape[0]), axis=1)
+    def process(self, frame):
+        zero_mean_frame = frame - frame.mean(axis=0, keepdims=True)
+        fft = np.fft.rfft(zero_mean_frame.T * np.hanning(frame.shape[0]), axis=1)
         abs_fft = np.abs(fft)
 
         return {
-            "sweep": sweep,
+            "frame": frame,
             "abs_fft": abs_fft,
         }
 
@@ -106,47 +102,44 @@ class PGUpdater:
     def __init__(self, sensor_config, processing_config, session_info):
         self.processing_config = processing_config
 
-        self.stepsize = sensor_config.stepsize
-        self.num_subsweeps = sensor_config.number_of_subsweeps
-        self.subsweep_rate = session_info["actual_subsweep_rate"]
+        self.downsampling_factor = sensor_config.downsampling_factor
+        self.sweeps_per_frame = sensor_config.sweeps_per_frame
+        sweep_rate = session_info["sweep_rate"]
         self.depths = utils.get_range_depths(sensor_config, session_info)
-        self.actual_stepsize_m = session_info["actual_stepsize"]
-        self.num_depths = self.depths.size
-        self.f_res = self.subsweep_rate / self.num_subsweeps
-        self.fft_x_scale = 100 * self.actual_stepsize_m
+        self.step_length = session_info["step_length_m"]
+        self.f_res = sweep_rate / self.sweeps_per_frame
+        self.fft_x_scale = 100 * self.step_length
 
-        self.smooth_max_f = self.subsweep_rate / self.num_subsweeps
+        self.smooth_max = utils.SmoothMax(
+            sweep_rate / self.sweeps_per_frame,
+            tau_grow=0,
+            tau_decay=0.5,
+            hysteresis=0.1,
+        )
 
         self.setup_is_done = False
 
     def setup(self, win):
         self.plots = []
         self.curves = []
-        for i in range(self.num_depths):
+        for i in range(self.depths.size):
             title = "{:.0f} cm".format(100 * self.depths[i])
             plot = win.addPlot(row=0, col=i, title=title)
             plot.showGrid(x=True, y=True)
-            plot.setYRange(-2**15, 2**15)
+            plot.setYRange(0, 2**16)
             plot.hideAxis("left")
             plot.hideAxis("bottom")
-            plot.plot(np.arange(self.num_subsweeps), np.zeros(self.num_subsweeps))
+            plot.plot(np.arange(self.sweeps_per_frame), 2**15 * np.ones(self.sweeps_per_frame))
             curve = plot.plot(pen=utils.pg_pen_cycler())
             self.plots.append(plot)
             self.curves.append(curve)
 
-        self.ft_plot = win.addPlot(row=1, col=0, colspan=self.num_depths)
+        self.ft_plot = win.addPlot(row=1, col=0, colspan=self.depths.size)
         self.ft_im = pg.ImageItem(autoDownsample=True)
         self.ft_im.setLookupTable(utils.pg_mpl_cmap("viridis"))
         self.ft_plot.addItem(self.ft_im)
         self.ft_plot.setLabel("bottom", "Depth (cm)")
-        self.ft_plot.getAxis("bottom").setTickSpacing(6 * self.stepsize, 6)
-
-        self.smooth_max = utils.SmoothMax(
-                self.smooth_max_f,
-                tau_grow=0,
-                tau_decay=0.5,
-                hysteresis=0.1,
-                )
+        self.ft_plot.getAxis("bottom").setTickSpacing(6 * self.downsampling_factor, 6)
 
         self.setup_is_done = True
         self.update_processing_config()
@@ -165,7 +158,7 @@ class PGUpdater:
 
         half_wavelength = 2.445e-3
         self.ft_im.resetTransform()
-        self.ft_im.translate(100 * (self.depths[0] - self.actual_stepsize_m / 2), 0)
+        self.ft_im.translate(100 * (self.depths[0] - self.step_length / 2), 0)
         if self.processing_config.show_speed_plot:
             self.ft_plot.setLabel("left", "Speed (m/s)")
             self.ft_im.scale(self.fft_x_scale, self.f_res * half_wavelength)
@@ -174,7 +167,7 @@ class PGUpdater:
             self.ft_im.scale(self.fft_x_scale, self.f_res * 1e-3)
 
     def update(self, data):
-        frame = data["sweep"]
+        frame = data["frame"]
 
         for i, ys in enumerate(frame.T):
             self.curves[i].setData(ys)
