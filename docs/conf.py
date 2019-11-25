66a7bad3c8d5d788c513a6232e6413f3adb9002a
Erik Månsson <erik.mansson@acconeer.com>
2019-11-25 14:07:53 +0100

    v3: Clean up docs conf
    Change-Id: I3005510602823142df08689929ea8107f762f6c4

--- docs/conf.py
+++ docs/conf.py
@@ -4,72 +4,63 @@ import os
 import sys
 
 
-conf_dir = os.path.dirname(os.path.abspath(__file__))
-root_dir = os.path.join(conf_dir, "..")
+conf_dir = os.path.dirname(os.path.realpath(__file__))
+root_dir = os.path.abspath(os.path.join(conf_dir, ".."))
 sys.path.append(root_dir)
 
-project = 'acconeer-python-exploration'
-copyright = '2019, Acconeer AB'
-author = 'Acconeer AB'
+project = "acconeer-python-exploration"
+copyright = "2019, Acconeer AB"
+author = "Acconeer AB"
 
-# version = ''  # The short X.Y version
-# release = ''  # The full version, including alpha/beta/rc tags
+# version = ""  # The short X.Y version
+# release = ""  # The full version, including alpha/beta/rc tags
 
 extensions = [
-    'sphinx.ext.mathjax',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.graphviz',
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.graphviz",
 ]
 
-autodoc_member_order = 'bysource'
+autodoc_member_order = "bysource"
 
 graphviz_dot_args = []
 graphviz_output_format = "svg"
 
-source_suffix = '.rst'
+source_suffix = ".rst"
 
-master_doc = 'index'
+master_doc = "index"
 
 language = None
 
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 pygments_style = None
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-htmlhelp_basename = 'acconeer-python-exploration-docs'
+htmlhelp_basename = "acconeer-python-exploration-docs"
 
 latex_elements = {
-    'papersize': 'a4paper',
-    'pointsize': '11pt',
+    "papersize": "a4paper",
+    "pointsize": "11pt",
 }
 
+# (startdocname, targetname, title, author, documentclass, toctree_only)
 latex_documents = [
-    (master_doc, 'acconeer-python-exploration.tex', 'acconeer-python-exploration Documentation',
-     'Acconeer', 'manual'),
+    (
+        master_doc,
+        "acconeer-python-exploration.tex",
+        "acconeer-python-exploration Documentation",
+        "Acconeer",
+        "manual",
+    ),
 ]
-
-man_pages = [
-    (master_doc, 'acconeer-python-exploration', 'acconeer-python-exploration Documentation',
-     [author], 1)
-]
-
-texinfo_documents = [
-    (master_doc, 'acconeer-python-exploration', 'acconeer-python-exploration Documentation',
-     author, 'acconeer-python-exploration', 'One line description of project.',
-     'Miscellaneous'),
-]
-
-epub_title = project
-
-epub_exclude_files = ['search.html']
 
 
 def setup(app):
-    app.add_stylesheet('css/custom.css')
+    app.add_stylesheet("css/custom.css")
 
 
 numfig = True
