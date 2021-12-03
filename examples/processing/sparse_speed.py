3bbb9eb676b6c2cf83cc7f976a766eff269530ee
Filip Alberius <Filip.Alberius@acconeer.com>
2021-12-03 14:50:15 +0100

    Update range interval for sparse_speed
    Change-Id: I3cd25778a638734a2d6bccd202a2139fb3def79d

--- examples/processing/sparse_speed.py
+++ examples/processing/sparse_speed.py
@@ -65,7 +65,7 @@ def get_sensor_config():
     config = et.configs.SparseServiceConfig()
     config.profile = et.configs.SparseServiceConfig.Profile.PROFILE_4
     config.sampling_mode = et.configs.SparseServiceConfig.SamplingMode.A
-    config.range_interval = [0.36, 0.54]
+    config.range_interval = [0.36, 0.60]
     config.downsampling_factor = 4
     config.sweeps_per_frame = 512
     config.hw_accelerated_average_samples = 60
