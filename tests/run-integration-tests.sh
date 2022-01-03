53e3baac7afcff20c37b625e4f0305384dac92ab
Fredrik Andersson <fredrik.andersson@acconeer.com>
2022-01-03 16:08:08 +0100

    test: ACC_BOARD parameter is not needed anymore
    Change-Id: Ib00339915f850230198472db074d9853f9ed71f1

--- tests/run-integration-tests.sh
+++ tests/run-integration-tests.sh
@@ -3,7 +3,7 @@
 short_hostname=$(hostname -s)
 echo -e "Running on host: $short_hostname\n"
 
-ACC_BOARD=emulation stash/out/customer/internal_sanitizer_x86_64/out/acc_exploration_server_a111 > output.txt &
+stash/out/customer/internal_sanitizer_x86_64/out/acc_exploration_server_a111 > output.txt &
 pid=$!
 
 pytest -v tests/integration --socket localhost 1
