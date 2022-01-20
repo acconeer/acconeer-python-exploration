cc3e2a12dd63fbf4fd1d92804cfbf8339de4b285
André Hedesand <andre.hedesand@acconeer.com>
2022-01-20 16:37:20 +0100

    Split `sparse_fft`
    Change-Id: Ib9ea8d859f2ef666efba96a6a8bebbbeaa02f24d

---
+++ src/acconeer/exptool/a111/algo/sparse_fft/processing.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+import acconeer.exptool as et
+
+
+def get_sensor_config():
+    config = et.configs.SparseServiceConfig()
+    config.profile = et.configs.SparseServiceConfig.Profile.PROFILE_3
+    config.sampling_mode = et.configs.SparseServiceConfig.SamplingMode.A
+    config.range_interval = [0.24, 0.48]
+    config.sweeps_per_frame = 64
+    config.sweep_rate = 3e3
+    config.hw_accelerated_average_samples = 60
+    return config
+
+
+class ProcessingConfiguration(et.configbase.ProcessingConfig):
+    VERSION = 1
+
+    show_data_plot = et.configbase.BoolParameter(
+        label="Show data",
+        default_value=True,
+        updateable=True,
+        order=0,
+    )
+
+    show_speed_plot = et.configbase.BoolParameter(
+        label="Show speed on FFT y-axis",
+        default_value=False,
+        updateable=True,
+        order=10,
+    )
+
+
+get_processing_config = ProcessingConfiguration
+
+
+class Processor:
+    def __init__(self, sensor_config, processing_config, session_info):
+        pass
+
+    def process(self, data, data_info):
+        frame = data
+
+        zero_mean_frame = frame - frame.mean(axis=0, keepdims=True)
+        fft = np.fft.rfft(zero_mean_frame.T * np.hanning(frame.shape[0]), axis=1)
+        abs_fft = np.abs(fft)
+
+        return {
+            "frame": frame,
+            "abs_fft": abs_fft,
+        }
