64fced79c66170d006a0d55b1f93674848ecd2f6
Erik Månsson <erik.mansson@acconeer.com>
2022-02-14 21:54:04 +0100

    portable: use pathlib instead of os.path
    Change-Id: Ie9b6128409d8355a088e705a90a603a20fe0c7b1

--- portable/make.py
+++ portable/make.py
@@ -1,35 +1,35 @@
-import os
 import shutil
 import zipfile
+from pathlib import Path
 
 import requests
 
 
 def main():
-    here = os.path.dirname(os.path.realpath(__file__))
-    package_dir = os.path.join(here, "package")
-    package_zip = os.path.join(here, "portable.zip")
+    here = Path(__file__).resolve().parent
+    package_dir = here / "package"
+    package_zip = here / "portable.zip"
 
-    if os.path.isfile(package_zip):
+    if package_zip.exists():
         return
 
-    path = os.path.join(package_dir, "tools", "get-pip.py")
-    if not os.path.isfile(path):
+    path = package_dir / "tools" / "get-pip.py"
+    if not path.exists():
         download("https://bootstrap.pypa.io/get-pip.py", path)
 
     url = "https://www.python.org/ftp/python/3.9.10/python-3.9.10-embed-amd64.zip"
     py_zip_name = url.split("/")[-1]
-    py_dir_name = os.path.splitext(py_zip_name)[0]
-    py_target_dir = os.path.join(package_dir, "tools", py_dir_name)
-    if not os.path.isdir(py_target_dir):
-        py_zip_path = os.path.join(here, py_zip_name)
-        if not os.path.isfile(py_zip_path):
+    py_dir_name = str(Path(py_zip_name).stem)
+    py_target_dir = package_dir / "tools" / py_dir_name
+    if not py_target_dir.exists():
+        py_zip_path = here / py_zip_name
+        if not py_zip_path.exists():
             download(url, py_zip_path)
 
         with zipfile.ZipFile(py_zip_path, "r") as z:
             z.extractall(py_target_dir)
 
-    shutil.make_archive(os.path.splitext(package_zip)[0], "zip", package_dir)
+    shutil.make_archive(package_zip.with_suffix(""), "zip", package_dir)
 
 
 def download(url, filename):
