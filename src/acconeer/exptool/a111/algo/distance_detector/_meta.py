f072b0cfdfc4de05454e5553e30783f0391452da
André Hedesand <andre.hedesand@acconeer.com>
2022-02-15 13:00:17 +0100

    Make distance detector single sensor
    Change-Id: I155e5f6779fd7df1ddf7b93dbc7b9c3f989d0f4b

--- src/acconeer/exptool/a111/algo/distance_detector/_meta.py
+++ src/acconeer/exptool/a111/algo/distance_detector/_meta.py
@@ -1,5 +1,4 @@
 from acconeer.exptool.a111.algo import ModuleFamily, ModuleInfo
-from acconeer.exptool.a111.algo.utils import multi_sensor_pg_updater, multi_sensor_processor
 
 from .plotting import PGUpdater
 from .processing import Processor, get_processing_config, get_sensor_config
@@ -8,12 +7,12 @@ from .processing import Processor, get_processing_config, get_sensor_config
 module_info = ModuleInfo(
     key="envelope_distance",
     label="Distance Detector (envelope)",
-    pg_updater=multi_sensor_pg_updater(PGUpdater),
+    pg_updater=PGUpdater,
     processing_config_class=get_processing_config,
     module_family=ModuleFamily.DETECTOR,
     sensor_config_class=get_sensor_config,
-    processor=multi_sensor_processor(Processor),
-    multi_sensor=True,
+    processor=Processor,
+    multi_sensor=False,
     docs_url=(
         "https://acconeer-python-exploration.readthedocs.io/"
         + "en/latest/processing/distance_detector.html"
