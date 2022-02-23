15b0e70bb07682a9d6c430fd8f930fdd25fd749f
Erik Månsson <erik.mansson@acconeer.com>
2022-02-23 09:04:31 +0100

    convert_to_csv: Remove dependency on load_record
    Change-Id: I85d55cebc3581917067bdfe20d3f3dafb8fbe71e

--- utils/convert_to_csv.py
+++ utils/convert_to_csv.py
@@ -1,12 +1,11 @@
 import argparse
 import csv
+import json
 import os
 
 import numpy as np
 
 import acconeer.exptool as et
-
-from load_record import print_record
 
 
 DESCRIPTION = """This is a command line utility that lets you convert
@@ -196,6 +195,59 @@ def _add_arguments(parser):
         default=False,
         help="Adds depth and sweep number info to the csv file",
     )
+
+
+def print_record(record):
+    print("Mode:", record.mode.name.lower())
+    print()
+    print(record.sensor_config)
+    print()
+    print("Session info")
+
+    for k, v in record.session_info.items():
+        print("  {:.<35} {}".format(k + " ", v))
+
+    print()
+    print("Data shape:", record.data.shape)
+    print("Data dtype:", record.data.dtype)
+    print()
+    print("Last data info (first sensor):")
+
+    for k, v in record.data_info[-1][0].items():
+        print("  {:.<35} {}".format(k + " ", v))
+
+    ts = record.sample_times
+    if ts is not None and ts.size >= 2:
+        print()
+        mean_dt = (ts[-1] - ts[0]) / (ts.size - 1)
+        mean_f = 1 / mean_dt
+        print("Mean sample rate (client side): {:.2f} Hz".format(mean_f))
+
+    print("\n")
+
+    print("Module (processing) key:", record.module_key)
+
+    if record.processing_config_dump is None:
+        print("No processing config dump")
+    else:
+        print("Processing config dump")
+        for k, v in json.loads(record.processing_config_dump).items():
+            print("  {:.<35} {}".format(k + " ", v))
+
+    print("\n")
+
+    m = {
+        "RSS version": record.rss_version,
+        "acconeer.exptool library version": record.lib_version,
+        "Timestamp": record.timestamp,
+    }
+
+    for k, v in m.items():
+        print("{:.<37} {}".format(k + " ", v))
+
+    if record.note:
+        print()
+        print("Note: " + str(record.note))
 
 
 if __name__ == "__main__":
