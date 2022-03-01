2abfa42ea2c7d2bb5152072feed2ab9217a2b0f1
André Hedesand <andre.hedesand@acconeer.com>
2022-03-01 16:04:43 +0100

    Add dummy gui/main.py
    Change-Id: I702541d5d9df2a8add295fcf9be09e4b9804e346

---
+++ gui/main.py
@@ -0,0 +1,33 @@
+try:
+    import acconeer.exptool as et
+
+    et_ver = et.__version__
+except ImportError:
+    et_ver = None
+
+SEPARATOR = "-" * 65
+
+print(SEPARATOR)
+print("Acconeer Exploration Tool is no longer run from `gui/main.py` since v4.")
+print()
+print(f"Installed version: {et_ver}")
+print(SEPARATOR)
+
+if et_ver is None or not et_ver.startswith("4"):
+    print("Install Acconeer Exploration Tool v4 with:")
+    print()
+    print("    python -m pip install acconeer-exptool[app]")
+    print()
+    print("    -- or --")
+    print()
+    print("    python -m pip .[app] (while standing in the root directory of the repository.)")
+    print()
+    print("And then run the app with")
+    print()
+    print("    python -m acconeer.exptool.app")
+    print()
+    print("Read more here: https://docs.acconeer.com/")
+else:
+    print("Acconeer Exploration Tool v4 is installed. Run it with:")
+    print()
+    print("    python -m acconeer.exptool.app")
