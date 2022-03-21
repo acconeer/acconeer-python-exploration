a7d77aef063a3829ea895d45adaaca6bb27f18c0
André Hedesand <andre.hedesand@acconeer.com>
2022-03-21 11:10:13 +0100

    Add check script for CHANGELOG
    Change-Id: I6f44057823e9792f922583c4c0532be05b46c941

---
+++ tools/check_changelog.py
@@ -0,0 +1,68 @@
+"""This script checks whether CHANGELOG refers to the same version as the current git tag."""
+import re
+import subprocess
+from pathlib import Path
+from typing import Optional
+
+
+def utf8_subprocess_output(*args: str):
+    return subprocess.check_output(args, encoding="utf-8").strip()
+
+
+def get_commit_sha_of_git_tag(git_tag: str):
+    return utf8_subprocess_output("git", "rev-list", "-n", "1", git_tag)
+
+
+def get_current_commit_sha():
+    return utf8_subprocess_output("git", "rev-parse", "--verify", "HEAD")
+
+
+def get_most_recent_git_tag():
+    return utf8_subprocess_output("git", "describe", "--abbrev=0", "--tags")
+
+
+def get_first_match_in_string(pattern: str, string: str, *, group: int = 0) -> Optional[str]:
+    """
+    Searches for the first match of `pattern` in `string`. Returns `group`:th group (default 0)
+    """
+    valid_group_numbers = range(0, re.compile(pattern).groups + 1)
+
+    if group not in valid_group_numbers:
+        raise ValueError(
+            f"`group` is out-of range. group={group} should be "
+            + f"in [{valid_group_numbers[0]}, {valid_group_numbers[-1]}]"
+        )
+
+    match = re.search(pattern, string)
+    return None if (match is None) else match.group(group)
+
+
+def main():
+    VERSION_PATTERN = r"v?\d+\.\d+\.\d+"
+    FILE_PATH = "CHANGELOG.md"
+
+    most_recent_tag = get_most_recent_git_tag()
+    most_recent_tag_commit_sha = get_commit_sha_of_git_tag(most_recent_tag)
+    current_commit_sha = get_current_commit_sha()
+
+    is_current_commit_tagged = most_recent_tag_commit_sha == current_commit_sha
+    if not is_current_commit_tagged:
+        exit(0)
+
+    print(f"Current commit ({current_commit_sha[:7]}) is tagged ({most_recent_tag}).")
+
+    changelog = Path(FILE_PATH)
+    first_match = get_first_match_in_string(VERSION_PATTERN, changelog.read_text())
+    print(f'Found "{first_match}" in {FILE_PATH}', end=" ... ")
+
+    is_exact_match = first_match == most_recent_tag
+    if is_exact_match:
+        print(f"Which matches {most_recent_tag} exactly.")
+        exit(0)
+    else:
+        print(f'Which does not match "{most_recent_tag}".')
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
