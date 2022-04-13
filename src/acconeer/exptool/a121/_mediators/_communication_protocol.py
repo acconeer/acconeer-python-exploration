abc96317ce67176426175ebc1a153e5006f986e5
André Hedesand <andre.hedesand@acconeer.com>
2022-04-13 11:11:40 +0200

    A121: Expand CommunicationProtocol interface
    Change-Id: Id6c5e060ea74744cd00195f9ec467bc77a43d30f

--- src/acconeer/exptool/a121/_mediators/_communication_protocol.py
+++ src/acconeer/exptool/a121/_mediators/_communication_protocol.py
@@ -1,27 +1,59 @@
 from __future__ import annotations
 
-from acconeer.exptool.a121 import Metadata, ServerInfo, SessionConfig
+from acconeer.exptool.a121 import Metadata, Result, ServerInfo, SessionConfig
 
 from typing_extensions import Protocol
 
 
 class CommunicationProtocol(Protocol):
+    end_sequence: bytes
+
     def get_system_info_command(self) -> bytes:
+        """The `get_system_info` command."""
         ...
 
     def get_system_info_response(self, bytes_: bytes) -> ServerInfo:
+        """Reads the response of `get_system_info` and parses it to a `ServerInfo`."""
         ...
 
     def get_sensor_info_command(self) -> bytes:
+        """The `get_sensor_info` command."""
         ...
 
     def get_sensor_info_response(self, bytes_: bytes) -> list[int]:
+        """Reads the response of `get_sensor_info` and returns a list of connected sensor_ids."""
         ...
 
     def setup_command(self, session_config: SessionConfig) -> bytes:
+        """The `setup` command."""
         ...
 
     def setup_response(
         self, bytes_: bytes, context_session_config: SessionConfig
     ) -> list[dict[int, Metadata]]:
+        """Parses the reponse of the `setup` command and parses it to an extended Metadata."""
+        ...
+
+    def start_streaming_command(self) -> bytes:
+        """The `start_streaming` command."""
+        ...
+
+    def start_streaming_response(self, bytes_: bytes) -> bool:
+        """Response of `start_streaming` command. Returns True if it was a success."""
+        ...
+
+    def get_next_header(self, bytes_: bytes) -> int:
+        """Parses the header of a data package. Returns the payload size."""
+        ...
+
+    def get_next_payload(self, bytes_: bytes) -> list[dict[int, Result]]:
+        """Parses the payload of the data package. Returns an extended Result."""
+        ...
+
+    def stop_streaming_command(self) -> bytes:
+        """The `stop_streaming` command"""
+        ...
+
+    def stop_streaming_response(self, bytes_: bytes) -> bool:
+        """Response of `stop_streaming` command. Returns True if it was a success."""
         ...
