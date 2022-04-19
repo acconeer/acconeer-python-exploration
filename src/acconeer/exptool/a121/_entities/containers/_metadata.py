0ce4b5420529610e5e1f9bd954c1313e2dc77b5a
André Hedesand <andre.hedesand@acconeer.com>
2022-04-19 17:37:45 +0200

    A121: Implement json/dict conversion for Metadata
    Change-Id: Ic3007a732c8ac4190d4f624f1fefb366adb738d6

--- src/acconeer/exptool/a121/_entities/containers/_metadata.py
+++ src/acconeer/exptool/a121/_entities/containers/_metadata.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import enum
+import json
 from typing import Any
 
 import attrs
@@ -26,15 +27,51 @@ class Metadata:
     _data_type: SensorDataType = attrs.field()
 
     def to_dict(self) -> dict[str, Any]:
-        raise NotImplementedError
+        d = attrs.asdict(self)
+        d["data_type"] = d.pop("_data_type")
+        return d
 
     @classmethod
     def from_dict(cls, d: dict) -> Metadata:
-        raise NotImplementedError
+        return cls(**d)
 
     def to_json(self) -> str:
-        raise NotImplementedError
+        return json.dumps(self, cls=MetadataEncoder)
 
     @classmethod
     def from_json(cls, json_str: str) -> Metadata:
-        raise NotImplementedError
+        return cls.from_dict(json.loads(json_str, cls=MetadataDecoder))
+
+
+class MetadataEncoder(json.JSONEncoder):
+    """Encoder that transforms a Metadata instance to a serializable
+    dict before any json transformation
+    """
+
+    def default(self, obj: Any) -> Any:
+        if isinstance(obj, Metadata):
+            metadata_dict = obj.to_dict()
+
+            # numpy arrays are not serializable.
+            # Since they are integer arrays, no precision will be lost
+            metadata_dict["subsweep_data_length"] = metadata_dict["subsweep_data_length"].tolist()
+            metadata_dict["subsweep_data_offset"] = metadata_dict["subsweep_data_offset"].tolist()
+            metadata_dict["data_type"] = metadata_dict["data_type"].name
+            return metadata_dict
+
+        return super().default(obj)
+
+
+class MetadataDecoder(json.JSONDecoder):
+    """Decoder that post-processes the dict (parsed from json) to better fit Metadata.from_dict"""
+
+    def decode(self, s: str) -> Any:  # type: ignore[override]
+        metadata_dict = super().decode(s)
+
+        # post process the parsed dict to have numpy-arrays instead of plain lists
+        metadata_dict["subsweep_data_length"] = np.array(metadata_dict["subsweep_data_length"])
+        metadata_dict["subsweep_data_offset"] = np.array(metadata_dict["subsweep_data_offset"])
+        # - || - for the enum value.
+        metadata_dict["data_type"] = SensorDataType[metadata_dict["data_type"]]
+
+        return metadata_dict
