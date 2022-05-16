0b7a51a18c9ee3b2f961bcbf100f6797a967bbc5
Erik Månsson <erik.mansson@acconeer.com>
2022-05-16 16:14:44 +0200

    A121: Add warning for unstable API
    Change-Id: I95ecbf41e000a1aedfa476123b2a32d930a9d9ea

--- src/acconeer/exptool/a121/__init__.py
+++ src/acconeer/exptool/a121/__init__.py
@@ -1,3 +1,5 @@
+import sys
+
 from ._cli import ExampleArgumentParser, get_client_args
 from ._core import (
     PRF,
@@ -24,3 +26,12 @@ from ._core import (
     save_record,
     save_record_to_h5,
 )
+
+
+if "pytest" not in sys.modules:
+    import warnings
+
+    warnings.warn(
+        "The a121 package is currently an unstable API and may change at any time.",
+        FutureWarning,
+    )
