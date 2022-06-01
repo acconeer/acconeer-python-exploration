7021f785196da2522dab74fa16df06f52c2c8741
Erik Månsson <erik.mansson@acconeer.com>
2022-06-01 08:56:16 +0200

    New app: Expand backend
    Change-Id: I0bc95673e59c58551c59187a6b1040ab04a8ebd0

--- src/acconeer/exptool/app/new/backend/_backend.py
+++ src/acconeer/exptool/app/new/backend/_backend.py
@@ -1,4 +1,10 @@
+from __future__ import annotations
+
 import multiprocessing as mp
+import queue
+from typing import Any, Tuple
+
+from ._model import Model
 
 
 class Backend:
@@ -21,26 +27,51 @@ class Backend:
 
     def stop(self):
         self._stop_event.set()
-        self.send("stop")
+        self._send(("stop", None))
         self._process.join()
         self._process.close()
 
-    def send(self, message):
+    def put_task(self, task):
+        self._send(("task", task))
+
+    def set_idle_task(self, task):
+        self._send(("set_idle_task", task))
+
+    def clear_idle_task(self):
+        self._send(("set_idle_task", None))
+
+    def _send(self, message: Tuple[str, Any]) -> None:
         self._send_queue.put(message)
 
-    def recv(self, message):
-        self._recv_queue.get(message)
+    def recv(self) -> Any:
+        return self._recv_queue.get()
 
 
 def process_program(recv_queue, send_queue, stop_event):
     try:
-        while not stop_event.is_set():
-            msg = recv_queue.get()
+        model = Model()
+        idle_task = None
 
-            if msg == "stop":
+        while not stop_event.is_set():
+            if idle_task is None:
+                msg = recv_queue.get()
+            else:
+                try:
+                    msg = recv_queue.get_nowait()
+                except queue.Empty:
+                    msg = ("task", idle_task)
+
+            cmd, arg = msg
+
+            if cmd == "stop":
                 break
 
-            ...
+            if cmd == "task":
+                model.execute_task(arg)
+            elif cmd == "set_idle_task":
+                idle_task = arg
+            else:
+                raise RuntimeError
     finally:
         recv_queue.close()
         send_queue.close()
---
+++ src/acconeer/exptool/app/new/backend/_model.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+
+class Model:
+    def __init__(self) -> None:
+        pass
+
+    def execute_task(self, task):
+        pass
