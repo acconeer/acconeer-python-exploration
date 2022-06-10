b09928b5f8efbab148d61c02b8a3c8a6b219eda8
Tommy Albing <tommy.albing@acconeer.com>
2022-06-10 10:01:38 +0200

    A121: Distance: Add logic to instanciate processors in the Aggregator class
    Change-Id: Ic74551b2e14355e0cd2822c35d80085f5382d660

--- src/acconeer/exptool/a121/algo/distance/_aggregator.py
+++ src/acconeer/exptool/a121/algo/distance/_aggregator.py
@@ -1,29 +1,69 @@
 from __future__ import annotations
 
-from typing import List, Optional
+from typing import Optional
 
 import attrs
 
 from acconeer.exptool import a121
-from acconeer.exptool.a121.algo.distance._processors import ProcessorConfig, ProcessorContext
+from acconeer.exptool.a121.algo.distance._processors import (
+    Processor,
+    ProcessorConfig,
+    ProcessorContext,
+)
 
 
 @attrs.frozen(kw_only=True)
-class ProcessorArgs:
-    sensor_config: a121.SensorConfig = attrs.field()
-    metadata: a121.Metadata = attrs.field()
+class ProcessorSpec:
     processor_config: ProcessorConfig = attrs.field()
-    subsweep_indexes: Optional[List[int]] = attrs.field(default=None)
-    context: Optional[ProcessorContext] = attrs.field(default=None)
+    group_index: int = attrs.field()
+    sensor_id: int = attrs.field()
+    subsweep_indexes: list[int] = attrs.field()
+    processor_context: Optional[ProcessorContext] = attrs.field(default=None)
+
+
+@attrs.frozen(kw_only=True)
+class AggregatorConfig:
+
+    pass
+
+
+@attrs.frozen(kw_only=True)
+class AggregatorResult:
+
+    pass
 
 
 class Aggregator:
-    """Aggregating processor
+    """Aggregating class
 
     Instantiates Processor objects based configuration from Detector.
 
     Aggregates result, based on selected peak sorting strategy, from underlying Processor objects.
     """
 
-    def __init__(self):
+    def __init__(
+        self,
+        session_config: a121.SessionConfig,
+        extended_metadata: list[dict[int, a121.Metadata]],
+        aggregator_config: AggregatorConfig,
+        specs: list[ProcessorSpec],
+    ):
+        self.aggregator_config = aggregator_config
+
+        self.processors: list[Processor] = []
+
+        for spec in specs:
+            metadata = extended_metadata[spec.group_index][spec.sensor_id]
+            sensor_config = session_config.groups[spec.group_index][spec.sensor_id]
+
+            processor = Processor(
+                sensor_config=sensor_config,
+                metadata=metadata,
+                processor_config=spec.processor_config,
+                subsweep_indexes=spec.subsweep_indexes,
+                context=spec.processor_context,
+            )
+            self.processors.append(processor)
+
+    def process(self, extended_result: list[dict[int, a121.Result]]) -> AggregatorResult:
         pass
