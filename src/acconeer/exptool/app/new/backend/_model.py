eacf851b34ad23ed232bc2f28ebab17122cb06fa
André Hedesand <andre.hedesand@acconeer.com>
2022-06-10 15:35:08 +0200

    New app: Make client connection more robust
    Change-Id: I4f799f7a6988afc57a8f8923c8fd3ffd66ddfbee

--- src/acconeer/exptool/app/new/backend/_model.py
+++ src/acconeer/exptool/app/new/backend/_model.py
@@ -26,6 +26,8 @@ class Model:
         try:
             self._execute_task(task)
         except Exception as e:
+            task_name, _ = task
+            self.task_callback(ErrorMessage(task_name, e))
             log.exception(e)
 
     def _execute_task(self, task: Task) -> None:
@@ -34,15 +36,29 @@ class Model:
             self.connect_client(**task_kwargs)
         elif task_name == "disconnect_client":
             self.disconnect_client(**task_kwargs)
+        elif self.backend_plugin is not None:
+            self.backend_plugin.execute_task(task=task)
         else:
             log.warning(f"Got unsupported task: {task_name}")
 
     def connect_client(self, client_info: a121.ClientInfo) -> None:
-        self.client = a121.Client(
-            ip_address=client_info.ip_address,
-            serial_port=client_info.serial_port,
-            override_baudrate=client_info.override_baudrate,
-        )
+        if self.client is None:
+            self.client = a121.Client(
+                ip_address=client_info.ip_address,
+                serial_port=client_info.serial_port,
+                override_baudrate=client_info.override_baudrate,
+            )
+        elif self.client.connected:
+            log.warn("Tried to connect when the Client is already connected. Will do nothing.")
+            return
+        elif self.backend_plugin is not None:
+            self.client = a121.Client(
+                ip_address=client_info.ip_address,
+                serial_port=client_info.serial_port,
+                override_baudrate=client_info.override_baudrate,
+            )
+            self.backend_plugin.attach_client(client=self.client)
+
         try:
             self.client.connect()
         except Exception as e:
@@ -62,9 +78,11 @@ class Model:
             )
             return
 
+        if self.backend_plugin is not None:
+            self.backend_plugin.detach_client()
+
         self.client.disconnect()
-        self.client = None
-        self.task_callback(Message("ok", "disconnect_client"))
+        self.task_callback(OkMessage("disconnect_client"))
 
     def _load_plugin(self, *, plugin: BackendPlugin) -> None:
         self.backend_plugin = plugin
