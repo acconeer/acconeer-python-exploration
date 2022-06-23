7d97f804d87ae77ad854060624d95ed714c0ec36
Erik Månsson <erik.mansson@acconeer.com>
2022-06-23 12:57:14 +0200

    A121: Move distance detector plugin
    Change-Id: I5278dbed5e3e40182bec624ae1669c9cd87c1bac

--- src/acconeer/exptool/app/new/plugin_loader.py
+++ src/acconeer/exptool/app/new/plugin_loader.py
@@ -4,7 +4,7 @@ from .app_model import Plugin
 
 
 def load_default_plugins() -> list[Plugin]:
-    from acconeer.exptool.a121.algo.distance._plugins._detector import DISTANCE_DETECTOR_PLUGIN
+    from acconeer.exptool.a121.algo.distance._detector_plugin import DISTANCE_DETECTOR_PLUGIN
     from acconeer.exptool.a121.algo.sparse_iq._plugin import SPARSE_IQ_PLUGIN
 
     return [
