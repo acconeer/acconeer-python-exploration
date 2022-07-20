c74be9f17fc4fd1b9063ee5b8f11095bd37fa133
Tommy Albing <tommy.albing@acconeer.com>
2022-07-20 14:40:46 +0200

    A121: Distance: Unit tests for the detector
    Change-Id: I97ce493982ff698f49d9281d0b91dee6a68bfae3

--- tests/unit/a121/algo/distance/test_detector.py
+++ tests/unit/a121/algo/distance/test_detector.py
@@ -1,3 +1,4 @@
+from acconeer.exptool import a121
 from acconeer.exptool.a121.algo import distance
 
 
@@ -26,3 +27,40 @@ def test_update_processor_mode():
             updated_spec.processor_config.processor_mode
             == distance.ProcessorMode.RECORDED_THRESHOLD_CALIBRATION
         )
+
+
+def test_m_to_points():
+    breakpoints_m = [0.5, 1.0, 1.5]
+    step_length = 4
+
+    actual_points = distance.Detector._m_to_points(
+        breakpoints_m=breakpoints_m, step_length=step_length
+    )
+
+    assert actual_points[0] == 200
+    assert actual_points[1] == 400
+    assert actual_points[2] == 600
+
+
+def test_select_prf():
+    breakpoint = 600
+    profile = a121.Profile.PROFILE_3
+
+    actual_PRF = distance.Detector._select_prf(breakpoint=breakpoint, profile=profile)
+
+    assert actual_PRF == a121.PRF.PRF_13_0_MHz
+
+
+def test_limit_step_length():
+    profile = a121.Profile.PROFILE_3
+    user_limit = 2
+
+    actual_step_length = distance.Detector._limit_step_length(
+        profile=profile, user_limit=user_limit
+    )
+    assert actual_step_length == 2
+
+    actual_step_length_no_user_limit = distance.Detector._limit_step_length(
+        profile=profile, user_limit=None
+    )
+    assert actual_step_length_no_user_limit == 12
