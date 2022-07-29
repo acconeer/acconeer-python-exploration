2c06eac249cbf9a3e9316cd667c4ee75b75eff46
André Hedesand <andre.hedesand@acconeer.com>
2022-07-29 13:20:55 +0200

    Small type-ignore fix.
    Change-Id: I8c016e60ce4efe70e052a656d03d76d079752b67

--- src/acconeer/exptool/a121/_core/entities/containers/common.py
+++ src/acconeer/exptool/a121/_core/entities/containers/common.py
@@ -2,4 +2,4 @@ import attrs
 import numpy as np
 
 
-attrs_ndarray_eq = attrs.cmp_using(eq=np.array_equal)  # type: ignore[attr-defined]
+attrs_ndarray_eq = attrs.cmp_using(eq=np.array_equal)  # type: ignore[call-arg]
