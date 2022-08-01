8be3b9085b0e54fceab0941ceb59d429390eed59
Elina Yrlid <elina.yrlid@acconeer.com>
2022-08-01 11:19:54 +0200

    Add ability to download and flash latest binary through CLI
    Feature: RS-5969
    Change-Id: I89eac3c0b330e5b53269c17b61eb3f1ef2f1ea6e

---
+++ src/acconeer/exptool/flash/_bin_fetcher.py
@@ -0,0 +1,166 @@
+# Copyright (c) Acconeer AB, 2022
+# All rights reserved
+
+from __future__ import annotations
+
+import cgi
+import logging
+import os
+import pickle
+import shutil
+import tempfile
+import zipfile
+from pathlib import Path
+from typing import Optional, Tuple
+
+import requests
+
+import platformdirs
+from bs4 import BeautifulSoup
+
+
+ET_DIR = Path(platformdirs.user_data_dir(appname="acconeer_exptool", appauthor="Acconeer AB"))
+CODENAME = "plugoneer"
+COOKIE_DIR = ET_DIR / CODENAME / "cookies"
+COOKIE_FILEPATH = COOKIE_DIR / "developer_page.pickle"
+REQUEST_URL = "https://developer.acconeer.com/log-in/"
+REFERER_URL = "https://developer.acconeer.com"
+
+
+log = logging.getLogger(__name__)
+
+
+def get_cookies() -> requests.cookies.RequestsCookieJar:
+    if os.path.exists(COOKIE_FILEPATH):
+        with open(COOKIE_FILEPATH, "rb") as handle:
+            cookie_jar = pickle.load(handle)
+        cookie_jar.clear_expired_cookies()
+        cookies = cookie_jar.get_dict()
+        if not cookies:
+            cookie_jar = None
+    else:
+        cookie_jar = None
+    return cookie_jar
+
+
+def _make_cookie_dir() -> None:
+    COOKIE_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def save_cookies(cookies: requests.cookies.RequestsCookieJar) -> None:
+    _make_cookie_dir()
+    with open(COOKIE_FILEPATH, "wb") as handle:
+        pickle.dump(cookies, handle)
+    log.debug("Login saved")
+
+
+def clear_cookies() -> None:
+    if os.path.exists(COOKIE_FILEPATH):
+        os.remove(COOKIE_FILEPATH)
+        log.debug("Login session removed")
+    else:
+        log.debug("No previous session saved")
+
+
+def login(email: str, password: str) -> requests.cookies.RequestsCookieJar:
+
+    with requests.Session() as session:
+        login_page = session.get(REQUEST_URL)
+        soup = BeautifulSoup(login_page.content, "html.parser")
+        token = soup.find("input", {"id": "CSRFToken-wppb"}).get("value")
+
+        credentials = {
+            "log": email,
+            "pwd": password,
+            "rememberme": "forever",
+            "wp-submit": "Log+In",
+            "wppb_login": "true",
+            "wppb_request_url": REQUEST_URL,
+            "wppb_redirect_priority": "normal",
+            "wppb_referer_url": REFERER_URL,
+            "CSRFToken-wppb": token,
+            "_wp_http_referer": "/log-in/",
+            "wppb_redirect_check": "true",
+        }
+        sw_page = session.post(REQUEST_URL, data=credentials, allow_redirects=False)
+        cookies = sw_page.cookies
+
+        return cookies
+
+
+def get_content(
+    cookies: requests.cookies.RequestsCookieJar,
+) -> Tuple[bool, requests.Session, requests.Response]:
+
+    with requests.Session() as session:
+        response = session.get(url=REFERER_URL, cookies=cookies.get_dict())
+
+        if response.url == REQUEST_URL:
+            return False, session, response
+        elif response.headers.get("Expires") is None:
+            return False, session, response
+        elif response.headers.get("Expires") is not None:
+            return True, session, response
+        else:
+            raise Exception
+
+
+def is_redirected_to_login_page(response: requests.Response) -> bool:
+    return response.url == REQUEST_URL
+
+
+def download(
+    session: requests.Session,
+    cookies: dict,
+    page: requests.Response,
+    path: str,
+    device: str,
+    acc_tc: bool = False,
+) -> Optional[str]:
+
+    if acc_tc is False:
+        return None
+
+    device = device.lower().replace(" ", "_")
+    soup = BeautifulSoup(page.content, "html.parser")
+    form = soup.select_one('form[action*="{}"]'.format(device))
+    zip_url = form.get("action")
+
+    download_value = form.select_one('[type="submit"]').get("value")
+
+    log.debug("File to download: {}".format(zip_url))
+
+    tc = {"tc_accepted": "1", "tc_submit": download_value}
+
+    r = session.post(zip_url, data=tc, allow_redirects=False, cookies=cookies.get_dict())
+
+    _, params = cgi.parse_header(r.headers["Content-Disposition"])
+    filename = params["filename"]
+
+    filepath = os.path.join(path, filename)
+
+    with open(filepath, "wb") as output_file:
+        output_file.write(r.content)
+        log.debug("Download completed at {}".format(filepath))
+
+    log.debug("Extracting files..")
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        bin_found = False
+        with zipfile.ZipFile(filepath, "r") as zipObject:
+            listOfFileNames = zipObject.namelist()
+            for fileName in listOfFileNames:
+                if fileName.endswith(".bin"):
+                    if bin_found:
+                        raise Exception("Multiple images in directory")
+                    bin_path = zipObject.extract(fileName, os.path.join(path, tmp_dir))
+                    bin_found = True
+            if bin_found is False:
+                raise Exception("No images found in directory")
+
+        dst_path = os.path.join(path, os.path.basename(bin_path))
+        shutil.move(bin_path, dst_path)
+
+        os.remove(filepath)
+
+    return dst_path
--- src/acconeer/exptool/flash/_flasher.py
+++ src/acconeer/exptool/flash/_flasher.py
@@ -1,13 +1,79 @@
 # Copyright (c) Acconeer AB, 2022
 # All rights reserved
 
+from __future__ import annotations
+
 import argparse
+import getpass
+import logging
 import platform
+import tempfile
+from typing import Optional
 
 import serial
 
 import acconeer.exptool as et
+from acconeer.exptool.flash._bin_fetcher import (
+    clear_cookies,
+    download,
+    get_content,
+    get_cookies,
+    is_redirected_to_login_page,
+    login,
+    save_cookies,
+)
 from acconeer.exptool.flash._products import PRODUCT_FLASH_MAP
+
+
+log = logging.getLogger(__name__)
+
+
+def _query_yes_no(question: str, default: str = "yes") -> bool:
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        print(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            print("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+
+
+def flash(bin_path: str, port: Optional[str] = None) -> None:
+
+    log.debug("Flashing image..")
+
+    flash_port = find_flash_port(port=port)
+    try:
+        flash_image(image_path=bin_path, flash_port=flash_port)
+    except Exception as e:
+        log.error(str(e))
+
+
+def _failed_autodetect():
+    raise Exception(
+        "Device type couldn't be autodetected. Specify device type by using the --device flag."
+    )
 
 
 def flash_image(image_path, flash_port):
@@ -50,7 +116,7 @@ def find_flash_port(port=None):
             elif port == pinfo[0].device:
                 flash_port = pinfo[0]
         if not flash_port:
-            print(f"{port} in not connected")
+            raise Exception(f"Port {port} is not connected")
     elif len(detected_ports) == 1:
         if isinstance(detected_ports[0], et.utils.USBDevice):
             flash_port = detected_ports[0]
@@ -73,10 +139,31 @@ def find_flash_port(port=None):
 def main():
     parser = argparse.ArgumentParser(description="Image Flasher")
     parser.add_argument(
-        "--port", dest="port", metavar="port", help="Serial port or USB device name"
+        "--port", dest="port", metavar="port", help="Serial port or USB device name."
     )
-    parser.add_argument("--image", "-i", required=True, help="Image file to flash")
+    parser.add_argument(
+        "--device",
+        "-d",
+        dest="device",
+        help="Device type. Only used if device type can't be autodetected.",
+        choices=["XC120"],
+    )
+    parser.add_argument(
+        "--clear", dest="clear", action="store_true", help="Clears saved login session."
+    )
 
+    image_group = parser.add_mutually_exclusive_group(required=True)
+    image_group.add_argument("--image", "-i", dest="image", help="Image file to flash")
+    image_group.add_argument(
+        "--fetch",
+        "-f",
+        dest="fetch",
+        action="store_true",
+        help=(
+            "Fetch latest image file from the web and flash it."
+            "Requires an Acconeer Developer Account."
+        ),
+    )
     verbosity_group = parser.add_mutually_exclusive_group(required=False)
     verbosity_group.add_argument(
         "-v",
@@ -98,6 +185,90 @@ def main():
 
     et.utils.config_logging(args)
 
-    flash_port = find_flash_port(args.port)
+    if args.clear:
+        clear_cookies()
 
-    flash_image(args.image, flash_port)
+    if args.fetch:
+        flash_port = find_flash_port(port=args.port)
+        if args.device is None:
+            if flash_port is not None:
+                if isinstance(flash_port, et.utils.USBDevice):
+                    flash_product = flash_port.name
+                else:
+                    flash_product = flash_port.product
+                if flash_product is None:
+                    _failed_autodetect()
+                device = flash_product
+            else:
+                _failed_autodetect()
+        else:
+            device = args.device
+
+        cookies = get_cookies()
+        new_login = False
+        login_succeed = False
+        while login_succeed is False:
+            promt = (
+                "To fetch the latest image you need to log into your Acconeer Developer account.\n"
+                "If you don't have an account you can create one here: "
+                "https://developer.acconeer.com/create-account/ \n"
+                "Lost your password? Recover it here: "
+                "https://developer.acconeer.com/recover-password/ \n"
+            )
+            if cookies is None:
+                print(promt)
+                email = input("Email: ")
+                password = getpass.getpass("Password: ")
+                new_login = True
+                cookies = login(email, password)
+
+            login_succeed, session, page = get_content(cookies)
+            if not login_succeed:
+                cookies = None
+
+                if is_redirected_to_login_page(page):
+                    print(
+                        "Request failed. Try downloading the image directly "
+                        "from https://developer.acconeer.com"
+                    )
+                else:
+                    print("\033[1;31mLogin failed.\033[1;0m")
+
+            else:
+                print("\033[1;32mLogin succeeded\033[1;0m")
+
+        if new_login:
+            if _query_yes_no("Save logged in session?"):
+                save_cookies(cookies)
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            bin_path = None
+            while bin_path is None:
+                bin_path = download(
+                    session=session,
+                    cookies=cookies,
+                    page=page,
+                    path=tmp_dir,
+                    device=device,
+                    acc_tc=_query_yes_no(
+                        (
+                            "\nDo you accept the terms and conditions listed at "
+                            "https://developer.acconeer.com/software-license-agreement/?"
+                        ),
+                        default=None,
+                    ),
+                )
+
+                if bin_path is None:
+                    print("You need to accept the terms and conditions to download the image.")
+
+            try:
+                print("Flashing...")
+                flash_image(bin_path, flash_port)
+            except ValueError:
+                print("No devices found, try connecting a device before flashing.")
+    elif args.image:
+        flash_port = find_flash_port(args.port)
+        flash_image(args.image, flash_port)
+    else:
+        raise Exception("No image provided")
