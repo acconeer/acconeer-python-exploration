75462154c273dcd13a2a3963e81fcbfba0267d8f
André Hedesand <andre.hedesand@acconeer.com>
2022-08-17 09:06:56 +0200

    Add dynamic prints to AppModelViewer
    Change-Id: Id38e242bff587e30425bdad3aea94d871cf8eaa0

--- src/acconeer/exptool/app/new/ui/app_model_viewer.py
+++ src/acconeer/exptool/app/new/ui/app_model_viewer.py
@@ -1,6 +1,10 @@
 # Copyright (c) Acconeer AB, 2022
 # All rights reserved
 
+from __future__ import annotations
+
+from typing import Any, Optional
+
 from PySide6.QtWidgets import QTextEdit
 
 from acconeer.exptool.app.new.app_model import AppModel
@@ -11,8 +15,25 @@ class AppModelViewer(QTextEdit):
         super().__init__()
         app_model.sig_notify.connect(self._update_text)
         self.setFontFamily("monospace")
-        self.setFixedSize(500, 200)
+        self.setMinimumSize(500, 200)
         self.setReadOnly(True)
+
+    @staticmethod
+    def _stringify_backend_state(backend_state: Optional[Any]) -> list[str]:
+        if backend_state is None:
+            return ["None"]
+
+        try:
+            members = backend_state.__slots__
+        except AttributeError:
+            members = backend_state.__dict__
+
+        public_members = [member for member in members if not member.startswith("_")]
+
+        return [
+            f">>> {attribute}\n" + f"{getattr(backend_state, attribute)}\n"
+            for attribute in public_members
+        ]
 
     def _update_text(self, app_model: AppModel) -> None:
         self.setText(
@@ -25,6 +46,9 @@ class AppModelViewer(QTextEdit):
                     f"PluginState:            {app_model.plugin_state}",
                     f"socket_connection_ip:   {app_model.socket_connection_ip}",
                     f"serial_connection_port: {app_model.serial_connection_port}",
+                    "",
+                    "backend_plugin_state:",
+                    *self._stringify_backend_state(app_model.backend_plugin_state),
                 ]
             )
         )
