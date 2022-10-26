acconeer/acconeer-python-exploration

commit d05bd1e9f42d3946069a6227eeb783b2bc9b7396

Isolate backend state updates
Change-Id: I1e7b2f4d9d2754999a6a1c619844300a53f46e35

src/acconeer/exptool/app/new/app_model/app_model.py
@@ -196,6 +196,9 @@ class AppModel(QObject):
     def broadcast(self) -> None:
         self.sig_notify.emit(self)
 
+    def broadcast_backend_state(self) -> None:
+        self.sig_backend_state_changed.emit(self.backend_plugin_state)
+
     def emit_error(self, exception: Exception, traceback_format_exc: Optional[str] = None) -> None:
         log.debug("Emitting error")
         self.sig_error.emit(exception, traceback_format_exc)
@@ -263,6 +266,7 @@ class AppModel(QObject):
         elif isinstance(message, BackendPluginStateMessage):
             log.debug("Got backend plugin state message")
             self.backend_plugin_state = message.state
+            self.broadcast_backend_state()
             self.broadcast()
         elif isinstance(message, StatusMessage):
             self.send_status_message(message.status)

src/acconeer/exptool/a121/algo/_plugins/processor/view_plugin.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 import abc
 import logging
-from typing import Generic, Type
+from typing import Generic, Optional, Type
 
 import qtawesome as qta
 
@@ -127,6 +127,28 @@ class ProcessorViewPluginBase(A121ViewPluginBase, Generic[ConfigT]):
         else:
             raise RuntimeError("Unknown message")
 
+    def on_backend_state_update(
+        self, backend_plugin_state: Optional[ProcessorBackendPluginSharedState]
+    ) -> None:
+        if backend_plugin_state is None:
+            self.session_config_editor.set_data(None)
+            self.processor_config_editor.set_data(None)
+            self.metadata_view.update(None)
+            self.perf_calc_view.update()
+        else:
+            assert isinstance(backend_plugin_state, ProcessorBackendPluginSharedState)
+            assert isinstance(
+                backend_plugin_state.processor_config, self.get_processor_config_cls()
+            )
+
+            self.session_config_editor.set_data(backend_plugin_state.session_config)
+            self.processor_config_editor.set_data(backend_plugin_state.processor_config)
+            self.metadata_view.update(backend_plugin_state.metadata)
+            self.perf_calc_view.update(
+                backend_plugin_state.session_config,
+                backend_plugin_state.metadata,
+            )
+
     def on_app_model_update(self, app_model: AppModel) -> None:
         self.session_config_editor.setEnabled(app_model.plugin_state == PluginState.LOADED_IDLE)
         self.processor_config_editor.setEnabled(app_model.plugin_state == PluginState.LOADED_IDLE)
@@ -137,26 +159,6 @@ class ProcessorViewPluginBase(A121ViewPluginBase, Generic[ConfigT]):
         )
         self.stop_button.setEnabled(app_model.plugin_state == PluginState.LOADED_BUSY)
         self.defaults_button.setEnabled(app_model.plugin_state == PluginState.LOADED_IDLE)
-
-        if app_model.backend_plugin_state is None:
-            self.session_config_editor.set_data(None)
-            self.processor_config_editor.set_data(None)
-            self.metadata_view.update(None)
-            self.perf_calc_view.update()
-        else:
-            assert isinstance(app_model.backend_plugin_state, ProcessorBackendPluginSharedState)
-            assert isinstance(
-                app_model.backend_plugin_state.processor_config, self.get_processor_config_cls()
-            )
-
-            self.session_config_editor.set_data(app_model.backend_plugin_state.session_config)
-            self.processor_config_editor.set_data(app_model.backend_plugin_state.processor_config)
-            self.metadata_view.update(app_model.backend_plugin_state.metadata)
-            self.perf_calc_view.update(
-                app_model.backend_plugin_state.session_config,
-                app_model.backend_plugin_state.metadata,
-            )
-
         self.session_config_editor.update_available_sensor_list(app_model._a121_server_info)
 
     @classmethod

src/acconeer/exptool/app/new/app_model/app_model_listener.py
@@ -1,6 +1,10 @@
 # Copyright (c) Acconeer AB, 2022
 # All rights reserved
-#
+
+from __future__ import annotations
+
+import typing as t
+
 from .app_model import AppModel
 
 
@@ -14,6 +18,7 @@ class AppModelListener:
     def __init__(self, app_model: AppModel) -> None:
         self.__app_model = app_model
         self.__app_model.sig_notify.connect(self.on_app_model_update)
+        self.__app_model.sig_backend_state_changed.connect(self.on_backend_state_update)
 
     def on_app_model_update(self, app_model: AppModel) -> None:
         """Hook for getting updates made to AppModel.
@@ -23,6 +28,15 @@ class AppModelListener:
         """
         pass
 
+    def on_backend_state_update(self, backend_plugin_state: t.Optional[t.Any]) -> None:
+        """Hook for getting updates made to the backend plugin.
+
+        AppModel calls this function when `AppModel.sig_backend_state_changed` is emitted,
+        i.e. when `AppModel.broadcast_backend_state` is invoked.
+        """
+        pass
+
     def stop_listening(self) -> None:
         """Stops listening to AppModel"""
         self.__app_model.sig_notify.disconnect(self.on_app_model_update)
+        self.__app_model.sig_backend_state_changed.disconnect(self.on_backend_state_update)

commit 21a2a971421b552caa7bd47e7328059c7f113018

Fix startup lag issue
Fixes visual lag introduced in d05bd1e9

Change-Id: I02597f1c37a0f5606093eb0236962ea37ec78224

src/acconeer/exptool/app/new/app_model/app_model.py
@@ -560,6 +560,7 @@ class AppModel(QObject):
         self.sig_load_plugin.emit(plugin)
         self.plugin = plugin
         self.broadcast()
+        self.broadcast_backend_state()
 
     def save_to_file(self, path: Path) -> None:
         log.debug(f"{self.__class__.__name__} saving to file '{path}'")