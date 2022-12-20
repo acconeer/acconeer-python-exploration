8fed2ec9f2a99f6f54caef377b16563bdf19d3fc
Anders Buhl <anders.buhl@acconeer.com>
2022-12-20 15:19:27 +0100

    Add python3.11 to nox test scope
    NOTE! This is not automatically tested. Python 3.11 has to be installed
    to be executed. Tests was successfully executed on Ubuntu 22.04
    
    Change-Id: I208e45db024e6b4096d554f4816f2b00c1dba5b8

--- noxfile.py
+++ noxfile.py
@@ -173,7 +173,7 @@ def docs_autobuild(session):
 
 
 @nox.session
-@nox.parametrize("python", ["3.7", "3.8", "3.9", "3.10"])
+@nox.parametrize("python", ["3.7", "3.8", "3.9", "3.10", "3.11"])
 def test(session):
     args = Parser().parse_args(session.posargs)
 
