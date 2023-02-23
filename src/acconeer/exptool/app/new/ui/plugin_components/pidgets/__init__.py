2604483be1f9a71dd234eb64ce06ca518dae9e55
André Hedesand <andre.hedesand@acconeer.com>
2023-02-23 15:19:26 +0100

    Move pidgets into its folder
    Change-Id: I85988a65ac404aee235fe39f83b9c0b8909de491

---
+++ src/acconeer/exptool/app/new/ui/plugin_components/pidgets/__init__.py
@@ -0,0 +1,29 @@
+# Copyright (c) Acconeer AB, 2023
+# All rights reserved
+
+from .pidgets import (
+    CheckboxParameterWidget,
+    CheckboxParameterWidgetFactory,
+    ComboboxParameterWidget,
+    ComboboxParameterWidgetFactory,
+    EnumParameterWidget,
+    EnumParameterWidgetFactory,
+    FloatParameterWidget,
+    FloatParameterWidgetFactory,
+    FloatSliderParameterWidget,
+    FloatSliderParameterWidgetFactory,
+    IntParameterWidget,
+    IntParameterWidgetFactory,
+    OptionalEnumParameterWidget,
+    OptionalEnumParameterWidgetFactory,
+    OptionalFloatParameterWidget,
+    OptionalFloatParameterWidgetFactory,
+    OptionalIntParameterWidget,
+    OptionalIntParameterWidgetFactory,
+    OptionalParameterWidget,
+    OptionalParameterWidgetFactory,
+    ParameterWidget,
+    ParameterWidgetFactory,
+    SensorIdParameterWidget,
+    SensorIdParameterWidgetFactory,
+)
