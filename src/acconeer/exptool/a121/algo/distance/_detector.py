acconeer/acconeer-python-exploration

commit cba763662a4d7c5041e9581ce889839407a7741c

A121: Distance: Add close range enable/disable.
Change-Id: I39817d724caf2131e202b68eb28ebdda3f6d1a60

src/acconeer/exptool/a121/algo/distance/_detector.py
@@ -288,6 +288,17 @@ class DetectorConfig(AlgoConfigBase):
     """Specifies the longest allowed profile. If no argument is provided, the highest possible
     profile without interference of direct leakage is used to maximize SNR."""
 
+    close_range_leakage_cancellation: bool = attrs.field(default=True)
+    """Enable close range leakage cancellation logic.
+
+    Close range leakage cancellation refers to the process of measuring close to the
+    sensor(<100mm) by first characterizing the direct leakage, and then subtracting it
+    from the measured sweep in order to isolate the signal component of interest.
+
+    The close range leakage cancellation process requires the sensor to be installed in its
+    intended geometry with free space in front of the sensor during detector calibration.
+    """
+
     signal_quality: float = attrs.field(default=15.0)
     """Signal quality. High quality equals higher HWAAS and better SNR but increase power
     consumption."""
@@ -1020,7 +1031,7 @@ class Detector(Controller[DetectorConfig, Dict[int, DetectorResult]]):
         close_range_transition_m = min_dist_m[a121.Profile.PROFILE_1]
 
         # Add close range group plan if applicable
-        if config.start_m < close_range_transition_m:
+        if config.close_range_leakage_cancellation and config.start_m < close_range_transition_m:
             plans[MeasurementType.CLOSE_RANGE] = cls._get_close_range_group_plan(
                 close_range_transition_m, config
             )
@@ -1101,7 +1112,10 @@ class Detector(Controller[DetectorConfig, Dict[int, DetectorResult]]):
             next_profile = transition_profiles[i + 1]
 
             if config.start_m < min_dist_m[next_profile] and min_dist_m[profile] < config.end_m:
-                start_m = max(min_dist_m[profile], config.start_m)
+                if i == 0 and not config.close_range_leakage_cancellation:
+                    start_m = config.start_m
+                else:
+                    start_m = max(min_dist_m[profile], config.start_m)
                 end_m = min(config.end_m, min_dist_m[next_profile])
                 has_neighbour = (
                     has_close_range_measurement or len(transition_subgroup_plans) != 0,

src/acconeer/exptool/a121/algo/distance/_detector_plugin.py
@@ -409,6 +409,9 @@ class ViewPlugin(DetectorViewPluginBase):
                 suffix=" m",
                 decimals=3,
             ),
+            "close_range_leakage_cancellation": pidgets.CheckboxPidgetFactory(
+                name_label_text="Enable close range leakage cancellation"
+            ),
             "max_step_length": pidgets.OptionalIntPidgetFactory(
                 name_label_text="Max step length",
                 checkbox_label_text="Set",

commit a4e26ffc6552489b833003460e9d6f1dde9ab7ca

A121: Distance: Bugfix: Incorrect end point with leakage cancellation
This bug was introduced with cba7636.

Prior to cba7636, a short end point resulted in the close range leakage
cancellation logic to be enforced.

With cba7636 implemented, it is possible to turn off the close range
cancellation logic, but not setting a short starting point. This change
fixes that.

Update test input/output-files as new logic result in different session
config.

Change-Id: I9003fd0179c6734624d329c03117903724a2357f

src/acconeer/exptool/a121/algo/distance/_detector.py
@@ -1097,6 +1097,10 @@ class Detector(Controller[DetectorConfig, Dict[int, DetectorResult]]):
         The transition region can consist of maximum two subsweeps, where the first utilize profile
         1 and the second profile 3. Whether both, one or none is used depends on the user provided
         detector config.
+
+        If close_range_leakage_cancellation is set to False, the first group plan should use
+        the user provided starting point as start, as there is no close range leakage cancellation
+        measurement.
         """
         transition_profiles = [
             profile
@@ -1111,14 +1115,22 @@ class Detector(Controller[DetectorConfig, Dict[int, DetectorResult]]):
             profile = transition_profiles[i]
             next_profile = transition_profiles[i + 1]
 
-            if config.start_m < min_dist_m[next_profile] and min_dist_m[profile] < config.end_m:
-                if i == 0 and not config.close_range_leakage_cancellation:
-                    start_m = config.start_m
-                else:
-                    start_m = max(min_dist_m[profile], config.start_m)
+            is_first_group_plan = len(transition_subgroup_plans) == 0
+            start_m = None
+            if (
+                not config.close_range_leakage_cancellation
+                and is_first_group_plan
+                and config.start_m < min_dist_m[next_profile]
+            ):
+                start_m = config.start_m
+
+            elif config.start_m < min_dist_m[next_profile] and min_dist_m[profile] < config.end_m:
+                start_m = max(min_dist_m[profile], config.start_m)
+
+            if start_m is not None:
                 end_m = min(config.end_m, min_dist_m[next_profile])
                 has_neighbour = (
-                    has_close_range_measurement or len(transition_subgroup_plans) != 0,
+                    has_close_range_measurement or not is_first_group_plan,
                     min_dist_m[next_profile] < end_m,
                 )
 