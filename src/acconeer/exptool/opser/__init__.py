a7c71459ee0f3c218c65a2716b55289b72f9ee38
André Hedesand <andre.hedesand@acconeer.com>
2024-04-12 14:06:24 +0200

    opser: Add try_deserialize function
    This is only a utility function, more suited when
    not called in a try-except context.
    
    Change-Id: Id9e7d0f0bc1f18145ab9e72c7bc042b3af12d37e

--- src/acconeer/exptool/opser/__init__.py
+++ src/acconeer/exptool/opser/__init__.py
@@ -1,5 +1,11 @@
-# Copyright (c) Acconeer AB, 2023
+# Copyright (c) Acconeer AB, 2023-2024
 # All rights reserved
 
 from . import builtin_persistors, core, optimizing_persistors
-from .api import deserialize, register_json_presentable, register_persistor, serialize
+from .api import (
+    deserialize,
+    register_json_presentable,
+    register_persistor,
+    serialize,
+    try_deserialize,
+)
--- src/acconeer/exptool/opser/api.py
+++ src/acconeer/exptool/opser/api.py
@@ -2,6 +2,7 @@
 # All rights reserved
 from __future__ import annotations
 
+import logging
 import typing as t
 
 import h5py
@@ -11,6 +12,7 @@ from .registry_persistor import RegistryPersistor
 
 
 _T = t.TypeVar("_T")
+_LOG = logging.getLogger(__name__)
 
 
 def serialize(
@@ -26,13 +28,30 @@ def serialize(
 
 def deserialize(group: h5py.Group, typ: t.Type[_T]) -> _T:
     """
-    Try to load and deserialize an object of type 'typ' from the specified group
+    Load and deserialize an object of type 'typ' from the specified group
+
+    Will raise an exception if anything goes wrong.
     """
     type_tree = core.create_type_tree(typ)
     loaded = RegistryPersistor(group, "./", type_tree).load()
     core.sanitize_instance(loaded, type_tree)
 
     return loaded  # type: ignore[no-any-return]
+
+
+def try_deserialize(group: h5py.Group, typ: t.Type[_T]) -> t.Optional[_T]:
+    """
+    Like deserialize, but returns None instead of an raising an exception when an error occurs.
+    """
+    try:
+        loaded = deserialize(group, typ)
+    except Exception as e:
+        _LOG.info(
+            f"Could not deserialize a {typ.__name__} from {group} due to exception: '{e!s}'."
+        )
+        return None
+    else:
+        return loaded
 
 
 register_persistor = RegistryPersistor.register_persistor
