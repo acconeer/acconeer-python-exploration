84ae6c1ed2b4fe8890a46bf8b7099041cf63be46
Peter Floderus <peter.floderus@acconeer.com>
2024-04-12 13:31:24 +0200

    A121: Make sure to initiate parking with default preset
    Change-Id: I531ecbefdf2f4cd391654642ce69e8167d8cedbe

--- src/acconeer/exptool/a121/algo/parking/_ref_app_plugin.py
+++ src/acconeer/exptool/a121/algo/parking/_ref_app_plugin.py
@@ -72,7 +72,7 @@ log = logging.getLogger(__name__)
 @attrs.mutable(kw_only=True)
 class SharedState:
     sensor_id: int = attrs.field(default=1)
-    config: RefAppConfig = attrs.field(factory=RefAppConfig)
+    config: RefAppConfig = attrs.field(factory=get_ground_config)
     context: RefAppContext = attrs.field(default=None)
 
 
