d4e0a099a6ff226a4470ad244c9f038c8f694d71
André Hedesand <andre.hedesand@acconeer.com>
2024-06-04 12:48:45 +0200

    Bump A121 version to v1.7.0
    Change-Id: I81255a3e2fd9134ed9574ef7f5a1b26cb007f72e

--- src/acconeer/exptool/a121/__init__.py
+++ src/acconeer/exptool/a121/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Acconeer AB, 2022-2024
 # All rights reserved
 
-SDK_VERSION = "1.6.0"
+SDK_VERSION = "1.7.0"
 
 # Make these visible under the a121 package to not break api
 from acconeer.exptool._core.communication.client import ClientError, ServerError
