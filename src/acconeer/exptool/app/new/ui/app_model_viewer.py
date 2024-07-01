5807bcfe27feef6ffdc24b3635b552e8c3246178
André Hedesand <andre.hedesand@acconeer.com>
2024-07-01 15:43:14 +0200

    Add signal log to AppModelViewer
    This is an extension to AppModelViewer that captures all
    emits of the majority of AppModel's signals and puts a
    string representation of the emitted object in a foldable view.
    
    This should make it easier to debug race conditions as
    all emits are shown. This is in contrast to only showing the
    latest one, which was done before.
    
    Change-Id: I8cac494c16c9da6879492478c02bfb95723435b6

--- src/acconeer/exptool/app/new/ui/app_model_viewer.py
+++ src/acconeer/exptool/app/new/ui/app_model_viewer.py
@@ -1,21 +1,31 @@
-# Copyright (c) Acconeer AB, 2022-2023
+# Copyright (c) Acconeer AB, 2022-2024
 # All rights reserved
 
 from __future__ import annotations
 
+import re
 from typing import Any, Optional
 
-from PySide6.QtWidgets import QTextEdit
+from PySide6.QtCore import SignalInstance
+from PySide6.QtGui import QStandardItem, QStandardItemModel
+from PySide6.QtWidgets import (
+    QAbstractItemView,
+    QPushButton,
+    QTabWidget,
+    QTextEdit,
+    QTreeView,
+    QVBoxLayout,
+    QWidget,
+)
 
 from acconeer.exptool.app.new.app_model import AppModel
 
 
-class AppModelViewer(QTextEdit):
+class _CurrentStateView(QTextEdit):
     def __init__(self, app_model: AppModel) -> None:
         super().__init__()
         app_model.sig_notify.connect(self._update_text)
         self.setFontFamily("monospace")
-        self.setMinimumSize(500, 330)
         self.setReadOnly(True)
         self.update_count = 0
 
@@ -59,3 +69,62 @@ class AppModelViewer(QTextEdit):
                 ]
             )
         )
+
+
+class _SignalLog(QWidget):
+    def __init__(self, app_model: AppModel) -> None:
+        super().__init__()
+
+        self._model = QStandardItemModel(self)
+        self._root = self._model.invisibleRootItem()
+
+        signal_items = (
+            self._connect_signal_to_log(app_model.sig_error),
+            self._connect_signal_to_log(app_model.sig_load_plugin),
+            self._connect_signal_to_log(app_model.sig_message_plot_plugin),
+            self._connect_signal_to_log(app_model.sig_message_view_plugin),
+            self._connect_signal_to_log(app_model.sig_status_file_path),
+            self._connect_signal_to_log(app_model.sig_status_message),
+            self._connect_signal_to_log(app_model.sig_rate_stats),
+            self._connect_signal_to_log(app_model.sig_frame_count),
+            self._connect_signal_to_log(app_model.sig_resource_tab_input_block_requested),
+            self._connect_signal_to_log(app_model.sig_backend_state_changed),
+        )
+
+        log_view = QTreeView()
+        log_view.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+        log_view.setHeaderHidden(True)
+        log_view.setModel(self._model)
+
+        clear_button = QPushButton("Clear log")
+
+        def remove_logs() -> None:
+            for i in signal_items:
+                i.removeRows(0, i.rowCount())
+
+        clear_button.pressed.connect(remove_logs)
+
+        layout = QVBoxLayout()
+        layout.addWidget(clear_button)
+        layout.addWidget(log_view)
+        self.setLayout(layout)
+
+    def _connect_signal_to_log(self, signal: SignalInstance) -> QStandardItem:
+        pattern = r"<PySide6.QtCore.SignalInstance ([a-zA-Z_]+).*"
+        if (match := re.match(pattern, str(signal))) is None:
+            signal_name = "unknown signal"
+        else:
+            (signal_name,) = match.groups("unknown signal")
+
+        item = QStandardItem(signal_name)
+        signal.connect(lambda x: item.appendRow(QStandardItem(str(x))))
+        self._root.appendRow(item)
+        return item
+
+
+class AppModelViewer(QTabWidget):
+    def __init__(self, app_model: AppModel) -> None:
+        super().__init__()
+        self.addTab(_CurrentStateView(app_model), "Current State")
+        self.addTab(_SignalLog(app_model), "Signal Log")
+        self.setMinimumSize(500, 500)
