81ef6092fc90858692fc5473088c41464820ae0c
André Hedesand <andre.hedesand@acconeer.com>
2024-07-18 15:10:46 +0200

    ET: Move argparser into main file
    Change-Id: I503b999401fc18d0125cc0ddcc718a33168a2d7a

--- src/acconeer/exptool/app/new/app.py
+++ src/acconeer/exptool/app/new/app.py
@@ -1,6 +1,7 @@
 # Copyright (c) Acconeer AB, 2022-2024
 # All rights reserved
 
+import argparse
 import ctypes
 import importlib.resources
 import sys
@@ -16,7 +17,6 @@ import pyqtgraph as pg
 from acconeer.exptool.app import resources
 from acconeer.exptool.utils import config_logging
 
-from ._argument_parser import ExptoolArgumentParser
 from .app_model import AppModel
 from .backend import Backend
 from .plugin_loader import import_and_register_plugin_module, load_plugins
@@ -25,7 +25,7 @@ from .ui import AppModelViewer, MainWindow
 
 
 def main() -> None:
-    parser = ExptoolArgumentParser()
+    parser = _ExptoolArgumentParser()
     args = parser.parse_args()
     config_logging(args)
 
@@ -80,6 +80,53 @@ def main() -> None:
     model.broadcast()
 
     sys.exit(app.exec())
+
+
+class _ExptoolArgumentParser(argparse.ArgumentParser):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.add_argument("--amv", action="store_true")
+        self.add_argument(
+            "--portable",
+            action="store_true",
+            help=argparse.SUPPRESS,  # makes option hidden
+        )
+        self.add_argument(
+            "--purge-config",
+            action="store_true",
+            help="Remove configuration files.",
+        )
+        self.add_argument(
+            "--plugin-module",
+            dest="plugin_modules",
+            metavar="module",
+            action="append",  # "append" => --plugin-module X --plugin-module Y => [X, Y]
+            help=(
+                "Allows you to load an arbitrary plugin given a python module "
+                + "(installed or in your working directory) NOTE! Accepted argument "
+                + "is not a path (e.g. not 'my_processor/latest/plugin.py'), it's a "
+                + "python module (e.g. 'my_processor.latest.plugin'). "
+                + "This option can be repeated."
+            ),
+        )
+
+        verbosity_group = self.add_mutually_exclusive_group(required=False)
+        verbosity_group.add_argument(
+            "-v",
+            "--verbose",
+            action="store_true",
+        )
+        verbosity_group.add_argument(
+            "-vv",
+            "--debug",
+            action="store_true",
+        )
+        verbosity_group.add_argument(
+            "-q",
+            "--quiet",
+            action="store_true",
+        )
 
 
 def _pixmap_to_icon(pixmap: QtGui.QPixmap) -> QtGui.QIcon:
