4810da25c103d692e5c7b2c8c223438790e7c919
André Hedesand <andre.hedesand@acconeer.com>
2024-12-03 10:05:35 +0100

    ui: Enable setting an icon on CollapsibleWidget
    This can be used to indicate errors in the collapsed widgets, even when
    they are collapsed.
    
    Change-Id: Ida7f736ca266042c98fbfa50c72f4a33b79c444c

--- src/acconeer/exptool/app/new/ui/components/collapsible_widget.py
+++ src/acconeer/exptool/app/new/ui/components/collapsible_widget.py
@@ -1,15 +1,17 @@
-# Copyright (c) Acconeer AB, 2023
+# Copyright (c) Acconeer AB, 2023-2025
 # All rights reserved
 
 from __future__ import annotations
 
 import typing as t
 
-from PySide6 import QtCore
-from PySide6.QtWidgets import QToolButton, QVBoxLayout, QWidget
+from PySide6 import QtCore, QtGui
+from PySide6.QtWidgets import QGridLayout, QLabel, QToolButton, QWidget
 
 
 class CollapsibleWidget(QWidget):
+    _ICON_SIZE = 28
+
     def __init__(self, label: str, widget: QWidget, parent: t.Optional[QWidget] = None) -> None:
         super().__init__(parent)
 
@@ -24,16 +26,29 @@ class CollapsibleWidget(QWidget):
         self.arrow_button.setChecked(False)
         self.arrow_button.toggled.connect(self._on_toggle)
 
-        self.setLayout(QVBoxLayout())
-        self.layout().setContentsMargins(0, 0, 0, 0)
-        self.layout().addWidget(self.arrow_button)
-        self.layout().addWidget(self.widget)
+        self.icon_label = QLabel()
+        self.set_icon(None)
+
+        layout = QGridLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.arrow_button, 0, 0)
+        layout.addWidget(self.icon_label, 0, 1)
+        layout.addWidget(self.widget, 1, 0, 1, -1)
+
+        self.setLayout(layout)
 
     def set_collapsed(self, collapsed: bool) -> None:
         self.widget.setVisible(not collapsed)
         self.arrow_button.setArrowType(
             QtCore.Qt.ArrowType.RightArrow if collapsed else QtCore.Qt.ArrowType.DownArrow
         )
+
+    def set_icon(self, icon: t.Optional[QtGui.QIcon]) -> None:
+        if icon is not None:
+            self.icon_label.setVisible(True)
+            self.icon_label.setPixmap(icon.pixmap(self._ICON_SIZE))
+        else:
+            self.icon_label.setVisible(False)
 
     def _on_toggle(self, checked: bool) -> None:
         self.set_collapsed(not checked)
