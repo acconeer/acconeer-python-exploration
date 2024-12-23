925fbd6e74bb608589e7d63dcc4972bb929e174d
André Hedesand <andre.hedesand@acconeer.com>
2024-12-23 11:32:41 +0100

    Add "Issue template" to unhandled exceptions
    Before this change, only the traceback was supplied in the copy-able
    text when an unforeseen issue occured in ET.
    
    In order to support users that experience issues, we've almost
    exclusively asked for output from both 'pip freeze' and 'python -V',
    both of which are now included and should hopefully streamline
    support/bug hunting.
    
    Change-Id: I226f21d2f9815eca17be0567119f6ad92f3a91c1

--- src/acconeer/exptool/app/new/ui/misc.py
+++ src/acconeer/exptool/app/new/ui/misc.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 import contextlib
 import logging
+import sys
 import traceback
 from typing import Iterator, Optional
 
@@ -17,6 +18,20 @@ from acconeer.exptool.app.new._exceptions import HandledException
 
 
 _LOG = logging.getLogger(__name__)
+
+
+def get_installed_packages_via_pip() -> Optional[list[str]]:
+    """Tries to extract information about installed packages"""
+    try:
+        try:
+            from pip._internal.operations.freeze import freeze
+        except ImportError:
+            from pip.operations.freeze import freeze  # type: ignore
+
+        return list(freeze())
+    except Exception as e:
+        _LOG.exception(e)
+        return None
 
 
 class ExceptionWidget(QMessageBox):
@@ -43,8 +58,39 @@ class ExceptionWidget(QMessageBox):
         except Exception:
             self.setInformativeText("<b>Unhandled error - please file a bug</b>")
 
+        # Monospaced detailed text.
+        self.setStyleSheet("QTextEdit { font-family: monospace; font-size: 9pt; }")
+
+        detailed_text = (
+            "Checklist before reporting a bug\n"
+            + "================================\n"
+            + "- [ ] I have read the 'Common Issues' without finding a solution\n"
+            + "      (See https://docs.acconeer.com/en/latest/exploration_tool/faq.html)\n"
+            + "- [ ] This issue occured in the latest release of Exploration Tool\n"
+            + "      (See https://pypi.org/project/acconeer-exptool/)\n"
+            + "- [ ] I have double-checked that the section 'Installed Packages' contains\n"
+            + "      rows on the format '<package name>==<version>'\n"
+            + "\n\n"
+        )
+
+        detailed_text += f"Python version\n==============\n{sys.version}\n\n\n"
+
+        installed_packages = get_installed_packages_via_pip()
+        detailed_text += "Installed Packages\n" + "==================\n"
+        if installed_packages is None:
+            detailed_text += (
+                "Please supply the output of 'pip freeze'\n"
+                + "(Could not extract information about installed packages)."
+            )
+        else:
+            detailed_text += "\n".join(installed_packages)
+        detailed_text += "\n\n\n"
+
         if traceback_str:
-            self.setDetailedText(traceback_str)
+            detailed_text += "Traceback\n" + "=========\n" + traceback_str
+
+        if detailed_text:
+            self.setDetailedText(detailed_text)
             copy_button = QPushButton(self)
             copy_button.setText("Copy details")
             self.addButton(copy_button, QMessageBox.ButtonRole.ActionRole)
@@ -62,7 +108,7 @@ class ExceptionWidget(QMessageBox):
 
     def resizeEvent(self, event: QtGui.QResizeEvent) -> None:
         super().resizeEvent(event)
-        self.setFixedWidth(500)
+        self.setMinimumWidth(600)
 
     def _on_copy_clicked(self) -> None:
         detailed_text = self.detailedText()
