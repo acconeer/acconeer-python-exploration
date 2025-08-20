4727c5832571fe736bb28c8dacf17404bc5191fa
André Hedesand <andre.hedesand@acconeer.com>
2025-08-20 15:10:15 +0200

    Move git hooks related stuff to internal_tools/
    Change-Id: Ibfba01c4bf682fdf3fe94850c3aada18f34ba279

--- internal_tools/install-git-hooks.sh
+++ internal_tools/install-git-hooks.sh
@@ -2,5 +2,5 @@
 GIT_TOP_DIR=$(git rev-parse --show-toplevel)
 
 echo "Installing pre-push hook"
-cp $GIT_TOP_DIR/utils/hooks/pre-push $GIT_TOP_DIR/.git/hooks/
+cp $GIT_TOP_DIR/internal_tools/hooks/pre-push $GIT_TOP_DIR/.git/hooks/
 chmod +x $GIT_TOP_DIR/.git/hooks/pre-push
